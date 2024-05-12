fa99489b1957df168aaa43d4a1b87cf0b76e1e12
Samson Udo <samsonudo@yahoo.com>
2024-05-12 21:48:24 +0100

    Apple Auth

--- lib/services/auth/index.dart
+++ lib/services/auth/index.dart
@@ -1,11 +1,51 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:crypto/crypto.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import '../../models/user.dart';
 
 class AuthService {
   static final auth = FirebaseAuth.instance;
   static final db = FirebaseFirestore.instance;
+
+  static String generateNonce([int length = 32]) {
+    final charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    final random = Random.secure();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+        .join();
+  }
+
+  static String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+
+  static Future<UserCredential> appleAuth() async {
+    final rawNonce = generateNonce();
+    final nonce = sha256ofString(rawNonce);
+
+    final appleCredential = await SignInWithApple.getAppleIDCredential(
+      scopes: [
+        AppleIDAuthorizationScopes.email,
+        AppleIDAuthorizationScopes.fullName,
+      ],
+      nonce: nonce,
+    );
+
+    final oauthCredential = OAuthProvider("apple.com").credential(
+      idToken: appleCredential.identityToken,
+      rawNonce: rawNonce,
+    );
+
+    return await auth.signInWithCredential(oauthCredential);
+  }
 
   static Future<UserCredential> login({
     required String email,
---
+++ lib/widgets/button/apple.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:green_luck/helper/snackbar.dart';
+import 'package:green_luck/pages/homePage/homepage.dart';
+import 'package:green_luck/services/auth/index.dart';
+import 'package:zap_sizer/zap_sizer.dart';
+import '../../theme/colors.dart';
+import '../../theme/text_style.dart';
+
+class AppleButton extends StatelessWidget {
+  const AppleButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isAndroid) {
+      return const SizedBox.shrink();
+    }
+
+    return SizedBox(
+      height: 6.h,
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () async {
+          try {
+            var cred = await AuthService.appleAuth();
+
+            if (cred.additionalUserInfo!.isNewUser) {
+              await AuthService.createAccount(
+                credential: cred,
+                displayName: cred.user!.displayName,
+              );
+            }
+
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (_) => const Homepage()),
+              (route) => false,
+            );
+          } on FirebaseAuthException catch (e) {
+            SnackbarHelper.displayToastMessage(
+              context: context,
+              message: e.message!,
+            );
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: primaryWhite,
+          side: const BorderSide(color: darkGreen, width: 2),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              FontAwesomeIcons.squareGooglePlus,
+              color: primaryBlack,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              'Apple',
+              style: mediumBold(primaryBlack),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
