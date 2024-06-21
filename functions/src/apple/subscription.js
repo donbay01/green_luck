52dd3cbb1465b3363d75d16d6876f2598c1c2fcb
Samson11 <samsonudo@yahoo.com>
2024-06-22 00:59:33 +0100

    fix to in-app purchase

--- functions/src/apple/subscription.js
+++ functions/src/apple/subscription.js
@@ -1,13 +1,24 @@
 const { onCall, HttpsError } = require('firebase-functions/v2/https')
 const { info } = require('firebase-functions/logger')
 
+const { getFirestore } = require('firebase-admin/firestore')
+const { creditUser } = require('../../helper/paystack')
+const db = getFirestore()
+
 exports.appleSubcription = onCall(async (req) => {
     if (!req.auth) {
         throw new HttpsError('unauthenticated', 'Need to logged in')
     }
 
     const { productID, purchaseID, serverVerificationData, localVerificationData } = req.data
-    info(req.data)
+    info(serverVerificationData)
 
-    return req.data
+    if (!serverVerificationData) {
+        return req.data
+    }
+
+    const { docs } = await db.collection('plans').where('apple_id', '==', productID).limit(1).get()
+    const plan = docs[0].data()
+
+    return creditUser(req.auth.uid, plan)
 })
